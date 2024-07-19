6b59abfa86d95d2c0f0f69516ba17d87990e1fb7
Udsanee PAKDEETRAKULWONG <udsanee@webmail.npru.ac.th>
2024-07-19 08:36:33 +0700

    Test three error types

---
+++ error/TestError.java
@@ -0,0 +1,12 @@
+public class TestError {
+public static void main(String[] args) {
+    //Experiment on syntax error by deleting semi colon
+    //System.out.println("Hello World")
+
+    //Experiment on runtime error by dividing with zero
+    //System.out.println(4/0);
+
+    //Experiment on logic error by wrong input  
+    //System.out.println(4.0/100); 
+}
+}  
