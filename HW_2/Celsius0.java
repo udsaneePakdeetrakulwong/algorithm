2dc655938c9ee004cdd877570be1b54589fd454d
Udsanee PAKDEETRAKULWONG <udsanee@webmail.npru.ac.th>
2024-07-20 21:58:20 +0700

    add more dotsss

--- HW_2/Celsius0.java
+++ HW_2/Celsius0.java
@@ -7,6 +7,6 @@ public class Celsius0 {
         float celsius = (fahrenheit - 32) * 5 / 9;
 
         // Display the result in celsius
-        System.out.println(fahrenheit + " Fahrenheit is equal to " + celsius + " Celsiusss.");
+        System.out.println(fahrenheit + " Fahrenheit is equal to " + celsius + " Celsiusss........");
     }
 }
