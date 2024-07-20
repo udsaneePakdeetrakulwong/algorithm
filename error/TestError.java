5788d7f1589e5fe914b439da28fa733e4648a8bf
Udsanee PAKDEETRAKULWONG <udsanee@webmail.npru.ac.th>
2024-07-20 21:30:24 +0700

    add ...

--- error/TestError.java
+++ error/TestError.java
@@ -1,7 +1,9 @@
+//package error;
+
 public class TestError {
 public static void main(String[] args) {
     //Experiment on syntax error by deleting semi colon
-    //System.out.println("Hello World")
+    System.out.println("Hello World");
 
     //Experiment on runtime error by dividing with zero
     //System.out.println(4/0);
